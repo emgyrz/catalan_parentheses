84c9d814aec6c2b6c27d3cf41e6560ee30279373
mz <mz@addreality.com>
2019-10-02 14:31:18 +0300

    upd

--- go.go
+++ go.go
@@ -3,18 +3,24 @@ package main
 import (
 	"fmt"
 	// "strings"
+	"time"
 	"os"
 	"strconv"
 )
+
+var closingBrackets string
 
 func main() {
 	count, err := strconv.Atoi(os.Args[1])
 	if err == nil {
 		fmt.Println(count)
 	}
+	s := time.Now()
 	variants := brackets(count)
+	t := time.Now().Sub(s)
 
-	fmt.Println(len(variants))
+	fmt.Println("time", t)
+	fmt.Println("variants", len(variants))
 	// fmt.Println(len(variants), strings.Join(variants, " , "))
 }
 
@@ -22,6 +28,10 @@ func main() {
 func brackets(n int) []string {
 	if n < 1 {
 		return nil
+	}
+	closingBrackets = ""
+	for i := 0; i < n; i++ {
+		closingBrackets += ")"
 	}
 	store := make([]string, 0)
 	br(n-1, n, "(", &store)
@@ -31,21 +41,15 @@ func brackets(n int) []string {
 
 func br(n, cl int, left string, store *[]string) {
 	if n == 0 {
-		*store = append(*store, left + getClosing(cl))
+		*store = append(*store, left+getClosing(cl))
 		return
 	}
 	br(n-1, cl, left+"(", store)
 	for i := 0; i < cl-n; i++ {
 		br(n-1, cl-i-1, left+getClosing(i+1)+"(", store)
 	}
-	//br(n-1, cl-1, left+")(", store)
 }
 
 func getClosing(n int) string {
-	closing := ""
-	for i :=0 ; i < n; i++ {
-		closing += ")"
-	}
-
-	return closing
+	return closingBrackets[:n]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 static O_BRACKET: char = '(';
 static C_BRACKET: char = ')';
 
+
 fn main() {
   let count: Vec<String> = std::env::args().collect();
   if count.len() < 2 {
@@ -9,69 +10,77 @@ fn main() {
   }
   let a: u32 = count[1]
     .parse()
-    .expect("cant parse first argument as usize");
+    .expect("cant parse first argument as uint");
 
   run(a);
 }
 
-fn gen(
-  results: &mut Vec<(String, String, usize)>,
-  count: usize,
-  start: &str,
-  opening: usize,
-  closing: usize,
-) {
-  let new_s_len = start.len() + 1;
-  let is_latest = new_s_len == count;
 
-  let mut new_s1 = String::from(start);
-  new_s1.push(O_BRACKET);
-  let opening_plus = opening + 1;
-  if is_latest {
-    let reversed = push_reversed(&new_s1);
-    results.push((new_s1, reversed, opening_plus - closing));
-  } else {
-    gen(results, count, &new_s1, opening_plus, closing);
-  }
 
-  if opening - closing > 0 {
-    let mut new_s2 = String::from(start);
-    new_s2.push(C_BRACKET);
-    let closing_plus = closing + 1;
-    if is_latest {
-      let reversed = push_reversed(&new_s2);
-      results.push((new_s2, reversed, opening - closing_plus));
-    } else {
-      gen(results, count, &new_s2, opening, closing_plus);
-    }
-  }
-}
 
 fn run(count: u32) {
   let count_us = count as usize;
-  let mut results = Vec::with_capacity(catalan_count(count) as usize);
+  let results_len = catalan_count(count) as usize;
+  let mut variants = Vec::with_capacity(results_len);
 
-  gen(&mut results, count_us, "(", 1, 0);
+  gen("(", count_us, 1, &mut variants);
 
-  let mut res = Vec::with_capacity(catalan_count(count) as usize);
+  let mut result = Vec::with_capacity(results_len);
 
   for i in 0..=count_us {
-    let its: Vec<_> = results.iter().filter(|it| it.2 == i).collect();
+    let its: Vec<_> = variants.iter().filter(|it| it.2 == i).collect();
     let len = its.len();
     for ind in 0..len {
       for idx in 0..len {
         let mut start = its[ind].0.clone();
         start.push_str(&its[idx].1);
-        res.push(start);
+        result.push(start);
       }
     }
   }
 
-  // println!("{:?} \n {}", res, res.len());
-  println!("\n {}", res.len());
+  // println!("{:?} \n {}", result, result.len());
+  println!("\n {}", result.len());
 }
 
-fn push_reversed(s: &str) -> String {
+
+
+fn gen(
+  start: &str,
+  total: usize,
+  opening: usize,
+  variants: &mut Vec<(String, String, usize)>,
+) {
+  let len = start.len();
+  let new_s_len = len + 1;
+  let is_latest = new_s_len == total;
+  let closing_len = len - opening;
+
+  let mut new_s1 = String::from(start);
+  new_s1.push(O_BRACKET);
+  let opening_plus = opening + 1;
+  if is_latest {
+    let reversed = reverse(&new_s1);
+    variants.push((new_s1, reversed, opening_plus - closing_len));
+  } else {
+    gen(&new_s1, total, opening_plus, variants);
+  }
+
+  if opening - closing_len > 0 {
+    let mut new_s2 = String::from(start);
+    new_s2.push(C_BRACKET);
+    let closing_plus = closing_len + 1;
+    if is_latest {
+      let reversed = reverse(&new_s2);
+      variants.push((new_s2, reversed, opening - closing_plus));
+    } else {
+      gen(&new_s2, total, opening, variants);
+    }
+  }
+}
+
+
+fn reverse(s: &str) -> String {
   s.chars()
     .rev()
     .map(|c| if c == '(' { ")" } else { "(" })
